a5677dd3e6b80bfa1c205b065d9a2827c4cccad5
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-10-07 16:27:21 -0700

    Added a failing test/ a few comments

--- WordWrapTest/WordWrapTest.cs
+++ WordWrapTest/WordWrapTest.cs
@@ -8,21 +8,32 @@ namespace WordWrapTest
 {
     public class WordWrapTests
     {
+        //Need to make the first test relevant to iteration
         [Fact]
         public void GetFirstLineOfText()
         {
             string input = "Hello World, this is the answer to everything!";
             int columnWidth = 11;
             string expected = "Hello World";
-            var answer = new Answer(input,columnWidth);
+            var answer = new Answer(input, columnWidth);
             var actual = answer.Words;
 
             actual.Should().Be(expected);
         }
 
-       
+        [Fact]
+        public void GetTwoLinesofText()
+        {
+            string input = "Hello there, this should result in at least two lines!";
+            int columnWidth = 11;
+            string expected = "Hello there\n,this shoul";
+            var answer = new Answer(input, columnWidth);
+            var actual = answer.Words;
 
-     
+            actual.Should().Be(expected);
+        }
+
+
     }
 
 }
