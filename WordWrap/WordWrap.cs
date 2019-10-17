af16ef86031b86829f58309303991e8ddec4a8e6
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-10-17 00:37:34 -0700

    WIP Working on refactor

--- WordWrap/WordWrap.cs
+++ WordWrap/WordWrap.cs
@@ -27,35 +27,51 @@ namespace WordWrap
             {
                 return words;
             }
-            else if (words.Length <= columnWidth * 2)//columnWith Index grow by 1 with each line
+            else 
             {
-                int index = 0;
-                string wordFirstLine = words.Substring(0, columnWidth);
-                string nextLine = "\n" + words.Substring(index += columnWidth, columnWidth);
-                return wordFirstLine + nextLine;
+              int index = 0;
+              string firstLine = words.Substring(0, columnWidth);
+              string nextLine ="";
+              int wordLength = words.Length;
+              while( index <= wordLength)
+              {
+                index += columnWidth;
+                nextLine = "\n" + words.Substring(index, columnWidth);
+                firstLine += nextLine;
+                wordLength -=columnWidth;  
+              }
+              return firstLine;
             }
-            else if (words.Length <= columnWidth * 3)
-            {
-                int index = 0;
-                string wordFirstLine = words.Substring(0, columnWidth);
-                index += columnWidth;
-                string nextLine = "\n" + words.Substring(index, columnWidth);
-                index += columnWidth; //line split index grows by the columnWith with each line
-                string nextNextLine = "\n" + words.Substring(index, columnWidth);
-                return wordFirstLine + nextLine + nextNextLine;
-            }
-            else
-            {
-                int index = 0;
-                string wordFirstLine = words.Substring(0, columnWidth);
-                index += columnWidth;
-                string nextLine = "\n" + words.Substring(index, columnWidth);
-                index += columnWidth;
-                string nextNextLine = "\n" + words.Substring(index, columnWidth);
-                index += columnWidth;
-                string nextNextNextLine = "\n" + words.Substring(index, columnWidth);
-                return wordFirstLine + nextLine + nextNextLine + nextNextNextLine;
-            }
+            // if (words.Length <= columnWidth * 2)//columnWith Index grow by 1 with each line
+            // {
+            //     int index = 0;
+            //     string wordFirstLine = words.Substring(0, columnWidth);
+            //     index += columnWidth;
+            //     string nextLine = "\n" + words.Substring(index, columnWidth);
+            //     return wordFirstLine + nextLine;
+            // }
+            // else if (words.Length <= columnWidth * 3)
+            // {
+            //     int index = 0;
+            //     string wordFirstLine = words.Substring(0, columnWidth);
+            //     index += columnWidth;
+            //     string nextLine = "\n" + words.Substring(index, columnWidth);
+            //     index += columnWidth; //line split index grows by the columnWith with each line
+            //     string nextNextLine = "\n" + words.Substring(index, columnWidth);
+            //     return wordFirstLine + nextLine + nextNextLine;
+            // }
+            // else
+            // {
+            //     int index = 0;
+            //     string wordFirstLine = words.Substring(0, columnWidth);
+            //     index += columnWidth;
+            //     string nextLine = "\n" + words.Substring(index, columnWidth);
+            //     index += columnWidth;
+            //     string nextNextLine = "\n" + words.Substring(index, columnWidth);
+            //     index += columnWidth;
+            //     string nextNextNextLine = "\n" + words.Substring(index, columnWidth);
+            //     return wordFirstLine + nextLine + nextNextLine + nextNextNextLine;
+            // }
 
         }
 
