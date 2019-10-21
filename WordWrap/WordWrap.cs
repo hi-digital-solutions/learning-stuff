3e4268abcb1d542337e67de6c462665d308b960b
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-10-21 15:59:49 -0700

    Revert "WIP Working on refactor"
    This reverts commit af16ef86031b86829f58309303991e8ddec4a8e6.

--- WordWrap/WordWrap.cs
+++ WordWrap/WordWrap.cs
@@ -28,6 +28,7 @@ namespace WordWrap
                 return words;
             }
             else if (words.Length <= columnWidth * 2)//columnWith Index grow by 1 with each line
+<<<<<<< HEAD
             {
                 int index = 0;
                 string firstLine = words.Substring(index, columnWidth);
@@ -58,6 +59,35 @@ namespace WordWrap
                    
               }
               return firstLine;
+=======
+            {
+                int index = 0;
+                string wordFirstLine = words.Substring(0, columnWidth);
+                string nextLine = "\n" + words.Substring(index += columnWidth, columnWidth);
+                return wordFirstLine + nextLine;
+            }
+            else if (words.Length <= columnWidth * 3)
+            {
+                int index = 0;
+                string wordFirstLine = words.Substring(0, columnWidth);
+                index += columnWidth;
+                string nextLine = "\n" + words.Substring(index, columnWidth);
+                index += columnWidth; //line split index grows by the columnWith with each line
+                string nextNextLine = "\n" + words.Substring(index, columnWidth);
+                return wordFirstLine + nextLine + nextNextLine;
+            }
+            else
+            {
+                int index = 0;
+                string wordFirstLine = words.Substring(0, columnWidth);
+                index += columnWidth;
+                string nextLine = "\n" + words.Substring(index, columnWidth);
+                index += columnWidth;
+                string nextNextLine = "\n" + words.Substring(index, columnWidth);
+                index += columnWidth;
+                string nextNextNextLine = "\n" + words.Substring(index, columnWidth);
+                return wordFirstLine + nextLine + nextNextLine + nextNextNextLine;
+>>>>>>> parent of af16ef8... WIP Working on refactor
             }
 
         }
