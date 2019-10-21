5c0d9b208593164f81e69dcdfa2e6cc23c423c80
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-10-21 16:04:35 -0700

    Revert Refactor of degenerate cases

--- WordWrap/WordWrap.cs
+++ WordWrap/WordWrap.cs
@@ -28,38 +28,6 @@ namespace WordWrap
                 return words;
             }
             else if (words.Length <= columnWidth * 2)//columnWith Index grow by 1 with each line
-<<<<<<< HEAD
-            {
-                int index = 0;
-                string firstLine = words.Substring(index, columnWidth);
-                index += columnWidth;
-                string nextLine = "\n" + words.Substring(index, columnWidth);
-                return firstLine + nextLine;
-            }
-            else 
-            {
-              int index = 0;
-              string firstLine = words.Substring(index, columnWidth);
-              string nextLine ="";
-              int wordLength = words.Length;
-              while( index <= wordLength)
-              {
-                index += columnWidth;
-                nextLine = "\n" + words.Substring(index, columnWidth);
-                firstLine += nextLine;
-                if(wordLength <= columnWidth)
-                {
-                    columnWidth = columnWidth - wordLength;  
-                    wordLength = 0;
-                }
-                else
-                {
-                    wordLength -= columnWidth; 
-                }
-                   
-              }
-              return firstLine;
-=======
             {
                 int index = 0;
                 string wordFirstLine = words.Substring(0, columnWidth);
@@ -87,7 +55,6 @@ namespace WordWrap
                 index += columnWidth;
                 string nextNextNextLine = "\n" + words.Substring(index, columnWidth);
                 return wordFirstLine + nextLine + nextNextLine + nextNextNextLine;
->>>>>>> parent of af16ef8... WIP Working on refactor
             }
 
         }
