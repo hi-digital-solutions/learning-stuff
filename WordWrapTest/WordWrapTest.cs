3bda6412ad5d89fe394dae40764b1a7b96923fb4
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-10-24 15:03:09 -0700

    Restart with most basic test

--- WordWrapTest/WordWrapTest.cs
+++ WordWrapTest/WordWrapTest.cs
@@ -20,55 +20,6 @@ namespace WordWrapTest
             actual.Should().Be(expected);
         }
 
-        [Fact]
-        public void GetOneLineOfText()
-        {
-            string input = "Hello World";
-            int columnWidth = 11;
-            string expected = "Hello World";
-            var answer = new Answer(input, columnWidth);
-            var actual = answer.Words;
-
-            actual.Should().Be(expected);
-        }
-
-        [Fact]
-        public void GetTwoLinesofTextWords()
-        {
-            string input = "Hello there, this shou";
-            int columnWidth = 11;
-            string expected = "Hello there\n, this shou";
-            var answer = new Answer(input, columnWidth);
-            var actual = answer.Words; 
-
-            actual.Should().Be(expected);
-        }
-
-        [Fact]
-        public void GetThreeLinesofTextWords()
-        {
-            string input = "Hello there, this should return t";
-            int columnWidth = 11;
-            string expected = "Hello there\n, this shou\nld return t";
-            var answer = new Answer(input, columnWidth);
-            var actual = answer.Words; 
-
-            actual.Should().Be(expected);
-        }
-
-        
-        [Fact]
-        public void GetFourLinesofTextWords()
-        {
-            string input = "Hello there, this should return the right am";
-            int columnWidth = 11;
-            string expected = "Hello there\n, this shou\nld return t\nhe right am";
-            var answer = new Answer(input, columnWidth);
-            var actual = answer.Words; 
-
-            actual.Should().Be(expected);
-        }
-
     }
 
 }
