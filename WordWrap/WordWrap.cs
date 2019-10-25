e2759845ad41595fb55debadc2a5d3fa217212e2
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-10-25 15:19:40 -0700

    Add Failing test for whitespace

--- WordWrap/WordWrap.cs
+++ WordWrap/WordWrap.cs
@@ -25,12 +25,13 @@ namespace WordWrap
 
         private string GetAnswer(string words, int columnWidth)
         {
-           if (words.Length <= columnWidth)
-           {
-               return words;
-           }
-           return "";
-            
+            var inputWords = words.Trim();
+            if (inputWords.Length <= columnWidth)
+            {
+                return inputWords;
+            }
+            return "";
+
         }
 
 
