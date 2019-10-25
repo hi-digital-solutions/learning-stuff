51a00001c84507020d9dd52d4ba481bd26bdb358
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-10-25 15:13:58 -0700

    Remove psuedo code

--- README.txt
+++ README.txt
@@ -8,3 +8,19 @@ You try to break lines at word boundaries.
 
 Like a word processor, break the line by replacing
 the last space in a line with a newline.
+
+\
+Notes
+   //Split the word string into a List
+               //Pass list to aggregator/reducer
+                    //Count length of first word in List
+                    //Check if word is longer than columWidth state
+                    //If no, 
+                        //add word to answer builder List (.Add(x))
+                        //Decrement starting columwidth by word length plus 1 (account for space)
+                        //Remove word from words List
+                    //Pass List again and repeat
+                //If yes 
+                    //add newline character to answerBuilder
+                    //Reset columnWidth state
+                    //Pass List again
--- WordWrap/WordWrap.cs
+++ WordWrap/WordWrap.cs
@@ -25,27 +25,9 @@ namespace WordWrap
 
         private string GetAnswer(string words, int columnWidth)
         {
-           if (words.Length < columnWidth)
+           if (words.Length <= columnWidth)
            {
                return words;
-           }
-           else if ( words.Length > columnWidth)
-           {
-               List<string> wordsList = words.Split(" ").ToList();
-               List<string> answerBuilder;
-               //Split the word string into a List
-               //Pass list to aggregator/reduces
-                    //Count length of first word in List
-                    //Check if word is longer than columWidth state
-                    //If no, 
-                        //add word to answer builder List (.Add(x))
-                        //Decrement starting columwidth by word length plus 1 (account for space)
-                        //Remove word from words List
-                    //Pass List again and repeat
-                //If yes 
-                    //add newline character to answerBuilder
-                    //Reset columnWidth state
-                    //Pass List again
            }
            return "";
             
