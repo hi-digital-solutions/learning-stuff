a4a61972717628fb5e1a7bd2c45bfd6a440bdc38
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-10-27 12:43:00 -0700

    Passing Test for two lines

--- WordWrap/WordWrap.cs
+++ WordWrap/WordWrap.cs
@@ -30,6 +30,26 @@ namespace WordWrap
             {
                 return inputWords;
             }
+            else if (inputWords.Length <= columnWidth *2)
+            {
+                var answerbuilder = new List<string>();
+                int columnState = columnWidth;
+                string answer;
+                foreach(var word in inputWords.Split(" "))
+                {
+                    if( word.Length <= columnState)
+                    {
+                        answerbuilder.Add($" {word}");
+                        columnState -= word.Length + 1;
+                    }
+                    else
+                    {
+                        answerbuilder.Add($"\n{word}");
+                        columnState = columnWidth;
+                    }
+                }
+                return answer = string.Join("", answerbuilder).Trim();
+            }
             return "";
 
         }
